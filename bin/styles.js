248502bd46e9dc4b4863f929196cd5d40acfb9d8
Jesús Antonio Ugaz Vegas <jesusugazv@gmail.com>
2020-12-13 00:46:49 -0500

    fix:correcion de bugs

--- bin/styles.js
+++ bin/styles.js
@@ -5,7 +5,6 @@
 var
     autoprefixer = require('autoprefixer'),
     debug = require('gulp-debug'),
-    less = require('gulp-less'),
     postcss = require('gulp-postcss'),
     program = require('commander'),
     sass = require('gulp-sass'),
@@ -48,10 +47,49 @@ program
             }
         });
 
-
+        console.log(input)
         return src(input, { allowEmpty: true })
             .pipe(debug({
                 title: 'commader-gulp-styles:'
+            }))
+            .pipe(sass())
+            .on('error', function (error) {
+                // tenemos un error 
+                util.log("Error Name:", error.name);
+                util.log("Error Code:", error.code);
+                util.log("Error Filename:", error.filename);
+                util.log("Error Line:", error.line);
+                util.log("Error Column:", error.column);
+                util.log("Error Msg", error.Msg);
+
+
+            })
+            .pipe(postcss(Plugins))
+            .pipe(dest(ouput))
+            .on('end', function () {
+                util.log('Done!');
+            });
+    })
+
+
+
+/* ######################## COMMANDER SCSS PRODUCTION ######################## */
+program
+    .command('prod:scss <input>')
+    .option("--sc [options]")
+    .action((input, options) => {
+        var input = options.input || options.parent.rawArgs;
+        var ouput = options.ouput || options.sc;
+        input = input.filter(function (index, value) {
+            if (index.slice((index.lastIndexOf(".") - 1 >>> 0) + 2) == "scss") {
+                return index;
+            }
+        });
+
+     
+        return src(input, { allowEmpty: true })
+            .pipe(debug({
+                title: 'commader-gulp-styles production:'
             }))
             .pipe(sass({
                 outputStyle: 'compressed'
@@ -72,14 +110,11 @@ program
             .on('end', function () {
                 util.log('Done!');
             });
-
-
-
-
     })
 
 /* ######################## COMMANDER STYLUS ######################## */
 /* "node ./bin/styles.js stylus \"frontend/src/static/styles/*.styl\" \"frontend/src/static/styles//*.styl\" --st \"docs/styles/\"" */
+
 program
     .command('stylus <input>')
     .option("--st [options]")
@@ -97,6 +132,45 @@ program
             .pipe(debug({
                 title: 'commader-gulp-styles:'
             }))
+            .pipe(stylus())
+            .on('error', function (error) {
+                // tenemos un error 
+                util.log("Error Name:", error.name);
+                util.log("Error Code:", error.code);
+                util.log("Error Filename:", error.filename);
+                util.log("Error Line:", error.line);
+                util.log("Error Column:", error.column);
+                util.log("Error Msg", error.Msg);
+
+
+            })
+            .pipe(poststylus(Plugins))
+            .pipe(dest(ouput))
+            .on('end', function () {
+                util.log('Done!');
+            });
+    })
+
+
+/* ######################## COMMANDER STYLUS PRODUCTION ######################## */
+program
+    .command('prod:stylus <input>')
+    .option("--st [options]")
+    .action((input, options) => {
+        var input = options.input || options.parent.rawArgs;
+        var ouput = options.ouput || options.st;
+        input = input.filter(function (index, value) {
+            if (index.slice((index.lastIndexOf(".") - 1 >>> 0) + 2) == "styl") {
+                return index;
+            }
+
+        });
+
+        return src(input, { allowEmpty: true })
+            .pipe(debug({
+                title: 'commader-gulp-styles production:'
+            }))
+
             .pipe(stylus({
                 compress: true
             }))
@@ -116,48 +190,6 @@ program
             .on('end', function () {
                 util.log('Done!');
             });
-
-
-
     })
-/* ######################## COMMANDER LESS ######################## */
-/* "node ./bin/styles.js less \"frontend/src/static/styles/*.styl\" \"frontend/src/static/styles//*.styl\" --lss \"docs/styles/\"" */
-program
-    .command('less <input>')
-    .option("--lss [options]")
-    .action((input, options) => {
-        var input = options.input || options.parent.rawArgs;
-        var ouput = options.ouput || options.st;
-        input = input.filter(function (index, value) {
-            if (index.slice((index.lastIndexOf(".") - 1 >>> 0) + 2) == "less") {
-                return index;
-            }
 
-        });
-
-        return src(input, { allowEmpty: true })
-            .pipe(debug({
-                title: 'commader-gulp-styles:'
-            }))
-            .pipe(less())
-            .on('error', function (error) {
-                // tenemos un error 
-                util.log("Error Name:", error.name);
-                util.log("Error Code:", error.code);
-                util.log("Error Filename:", error.filename);
-                util.log("Error Line:", error.line);
-                util.log("Error Column:", error.column);
-                util.log("Error Msg", error.Msg);
-
-
-            })
-            .pipe(postcss(Plugins))
-            .pipe(dest(ouput))
-            .on('end', function () {
-                util.log('Done!');
-            });
-
-
-
-    })
 program.parse(process.argv);
